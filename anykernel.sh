bba2f1cbfe7ba776b46955d7e39d2135aa15199f
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-02-08 11:00:03 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Neophyte Kernel by k4ngcaribug @ telegram
+kernel.string=AnajaniLaurens
 do.devicecheck=1
 do.modules=0
 do.systemless=1
